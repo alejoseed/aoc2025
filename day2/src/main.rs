a4e622222bdca68d394e45ee683e2919c5420c5f
Alejandro Palmar <alejandro@abltech.com>
2025-12-02 11:25:40 -0500

    Day 2

---
+++ day2/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let path = "/home/alejoseed/aoc2025/day2/src/1.input";
+    let file_buf = std::fs::read_to_string(path).expect("Could not read file");
+    
+    let ranges: Vec<(i64, i64)> = Vec::new();
+}
